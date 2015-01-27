1af9166437c58be7c07473d45b8f08ac057b5a8b
Hezekiah M. Carty <hez@0ok.org>
2015-01-27 12:20:28 -0500

    Possible memory leak fix

--- src/spatial_reference.ml
+++ src/spatial_reference.ml
@@ -30,15 +30,15 @@ let destroy_spatial_reference =
 
 let export_to_proj4 =
   Lib.c "OSRExportToProj4"
-    (t @-> ptr string @-> returning err)
+    (t @-> ptr (ptr char) @-> returning err)
 
 let export_to_wkt =
   Lib.c "OSRExportToWkt"
-    (t @-> ptr string @-> returning err)
+    (t @-> ptr (ptr char) @-> returning err)
 
 let export_to_pretty_wkt =
   Lib.c "OSRExportToPrettyWkt"
-    (t @-> ptr string @-> int @-> returning err)
+    (t @-> ptr (ptr char) @-> int @-> returning err)
 
 let free =
   Lib.c "OGRFree"
@@ -59,13 +59,27 @@ let make kind spec =
   in
   sr
 
+(* Based on dbuenzli's string from char array example *)
+let string_of_char_ptr p =
+  let b = Buffer.create 256 in
+  let continue = ref true in
+  let i = ref 0 in
+  while !continue do
+    let c = !@(p +@ !i) in
+    if c = '\x00' then continue := false else Buffer.add_char b c;
+    incr i
+  done;
+  Buffer.contents b
+
 let to_proj4 sr =
-  let s = allocate string "" in
+  let s = allocate (ptr char) (from_voidp char null) in
   export_to_proj4 sr s;
-  !@ s
+  let result = string_of_char_ptr !@s in
+  free (to_voidp !@s);
+  result
 
 let to_wkt ?(pretty = false) ?(simplify = false) sr =
-  let s = allocate string "" in
+  let s = allocate (ptr char) (from_voidp char null) in
   let f x =
     if pretty then (
       export_to_pretty_wkt sr x (if simplify then 1 else 0)
@@ -75,4 +89,6 @@ let to_wkt ?(pretty = false) ?(simplify = false) sr =
     )
   in
   f s;
-  !@ s
+  let result = string_of_char_ptr !@s in
+  free (to_voidp !@s);
+  result
