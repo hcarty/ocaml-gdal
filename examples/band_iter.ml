b88f742c1ee7b517346bc36a22d277f8dbc8b6ef
Hezekiah M. Carty <hez@0ok.org>
2015-03-07 22:33:14 -0500

    Fix usage message

--- examples/band_iter.ml
+++ examples/band_iter.ml
@@ -2,7 +2,7 @@ open Gdal
 
 let () =
     if Array.length Sys.argv <> 2 then (
-      Printf.eprintf "Usage: %s [source file]" Sys.argv.(0);
+      Printf.eprintf "Usage: %s [source file]\n" Sys.argv.(0);
       exit 1
     );
   let file = Sys.argv.(1) in
